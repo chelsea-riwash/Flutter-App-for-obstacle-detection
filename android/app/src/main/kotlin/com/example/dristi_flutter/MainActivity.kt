82342d8d55662a753d47dd4ac368d1148cd3ddbc
chelsea-riwash <riwazn@gmail.com>
2022-12-28 20:39:32 +0545

    First Commit

---
+++ android/app/src/main/kotlin/com/example/dristi_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dristi_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+// import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CameraDescription>? cameras;
+
+  CameraController? controller;
+
+  @override
+  void initState() {
+    loadCamera();
+    super.initState();
+  }
+
+  loadCamera() async {
+    cameras = await availableCameras();
+    if (cameras != null) {
+      controller = CameraController(cameras![0], ResolutionPreset.max);
+      //cameras[0] = first camera, change to 1 to another camera
+
+      controller!.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+      });
+    } else {
+      print("No any camera found");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DRISTI"),
+        backgroundColor: Color(0xFF2899bd),
+      ),
+      body: Container(
+          child: Column(children: [
+        Container(
+            child: controller == null
+                ? Center(child: Text("Loading Camera..."))
+                : !controller!.value.isInitialized
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : CameraPreview(controller!)),
+      ])),
+    );
+  }
+}
